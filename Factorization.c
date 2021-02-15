d9d4e4c011c0d797ac651200a55355fc8387ebe1
nared456 <78350512+nared456@users.noreply.github.com>
2021-02-15 17:46:07 +0700

    Fact_6320503041

--- Factorization.c
+++ Factorization.c
@@ -1,1 +1,21 @@
-
+#include<stdio.h>
+int main()
+{
+    int k,x=2,i=0;
+    scanf("%d",&k);
+    if(k==1)
+    {
+        printf("1\n");
+    }
+    while(k!=1)
+    {
+        while(k%x==0)
+        {
+            printf("%d\n",x);
+            k /= x;
+        }
+        x++;
+    }
+    printf("%d",i);
+    return 0;
+}
