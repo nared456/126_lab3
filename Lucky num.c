0fe94caaa867385225749b61930d44443b334267
nared456 <78350512+nared456@users.noreply.github.com>
2021-02-15 19:09:30 +0700

    edit_6320503041

--- Lucky num.c
+++ Lucky num.c
@@ -1,1 +1,21 @@
-
+#include<stdio.h>
+int main()
+{
+    int num,p=0,x=2,n=0,tmp;
+    scanf("%d",&num);
+    tmp=num;
+    while(tmp<1)
+    {
+        while(tmp%x==0)
+        {
+            tmp /= x;
+            n++;
+        }
+        x++;
+    }
+    if(n==3)
+        printf("%d is a Lucky Number.",num);
+    else
+        printf("%d is not a Lucky Number.",num);
+    return 0;
+}
--- prime_t.c
+++ prime_t.c
@@ -1,1 +1,26 @@
+#include<stdio.h>
+int main()
+{
+    long long num,i;
+    int p;
+    scanf("%lld",&num);
+    for(num;num>=0;num--)
+    {
+        p = 1;
+        for(i=2;i<num;i++)
+        {
+            if(num%i==0)
+            {
+                p=0;
+                break;
+            }
+        }
+        if(p==1)
+        {
+            printf("%lld",num);
+            break;
+        }
 
+    }
+    return 0;
+}
